bbf2200deee310d669021ce66f3833131081e69c
Gamvir darlami <darlamiswornim@gmail.com>
2022-07-01 09:54:08 +0545

    ptest

---
+++ chapter 3/NestedIfElsestatement.py
@@ -0,0 +1,49 @@
+# username = 'admin'
+# password = 'admin'
+
+# if username == 'admin':
+#     if password == 'admin':
+#         print("you are logged in")
+#     else:
+#         print("incorrect password")    
+# else:
+#     print("invalid username")    
+
+# x = 45
+# y = 65
+# z = 76
+
+# if x > y:
+#     if x > z:
+#         print("x is greater ")
+#     else:
+#         print("z is greater")    
+# else:
+#     if y > z:
+#         print("y is greater")        
+#     else:
+#         print("z is greater")
+
+# age = 0
+# age = int(input("enter the age:"))
+# if age > 18 or age < 40:
+#     print("you are invited")
+#     if age > 25 and age < 40:
+#         print("VIP treatment should be provided")
+#     else:
+#         print("VIP treatment should not")    
+# else:
+#     print("you are not invited ")     
+
+# x = int(input("enter the number:"))
+# if x % 2 == 0:
+#     print("number is even")
+# else:
+#     print("number is odd")    
+
+x = int(input("enter the number:"))
+if x % 3 == 0:
+    if x % 5 ==0:
+        print("Number entered is divisible")
+else:
+    print("not divisible")        
--- chapter 3/age .py
+++
@@ -1,7 +0,0 @@
-age = int(input("enter ur age :"))
-if age < 18 or age >= 40:
-    print("you are not welcome at the party ")
-elif age >= 18 or age < 40:
-    print("you are welcome at the party")
-
-
